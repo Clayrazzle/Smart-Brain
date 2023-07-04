b5a62fa209a47fe3373da6e985aa4fdb7a70fbe5
Clay Randall Smith <Clayrazzle@Clays-MacBook-Pro.local>
2023-07-04 16:47:18 +0900

    homepage

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('https://smart-brain-api-vuhl.onrender.com/register', {
+    fetch('https://smartbackend-p304.onrender.com', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
