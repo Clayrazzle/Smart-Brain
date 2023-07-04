4b3eaf621e793f2aadd77145bcd970692d4e3cda
Clay Randall Smith <Clayrazzle@Clays-MacBook-Pro.local>
2023-07-04 17:15:32 +0900

    fetch

--- src/App.js
+++ src/App.js
@@ -76,7 +76,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('https://smartbackend-p304.onrender.come', {
+          fetch('https://smartbackend-p304.onrender.com', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
