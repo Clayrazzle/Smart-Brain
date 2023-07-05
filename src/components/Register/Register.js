4e34ac3c9670c8a2b056653c61df33e369165bf6
Clay Randall Smith <Clayrazzle@Clays-MacBook-Pro.local>
2023-07-05 15:09:55 +0900

    port3

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('https://smartbackend-p304.onrender.com/register', {
+    fetch('https://smartbackend-p304.onrender.com/', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
