1ba558ab46f9eac9a4c6db8d1578bd744bdcbb01
Clay Randall Smith <Clayrazzle@Clays-MacBook-Pro.local>
2023-07-08 10:02:54 +0900

    samus

--- src/App.js
+++ src/App.js
@@ -66,7 +66,7 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-      fetch('https://smartbackend-p304.onrender.com/imageurl', {
+      fetch('https://samusbackend.onrender.com/imageurl', {
         method: 'post',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({
@@ -76,7 +76,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('https://smartbackend-p304.onrender.com/image', {
+          fetch('https://samusbackend.onrender.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
