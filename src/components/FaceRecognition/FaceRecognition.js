f173908fbac7e9a6ce3d7af6cc2d112441eb32fc
Clay Randall Smith <Clayrazzle@Clays-MacBook-Pro.local>
2024-01-29 18:09:14 +0900

    multiple detection1

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -19,7 +19,7 @@ const FaceRecognition = ({ imageUrl, boxes }) => {
     <div className='center ma'>
       <div className='absolute mt2'>
         <img id='inputimage' alt='' src={imageUrl} width='500px' height='auto' />
-        {boxes.map((box, index) => (
+        {boxes && boxes.map((box, index) => (
           <div key={index} className='bounding-box' style={{ top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol }}></div>
         ))}
       </div>
