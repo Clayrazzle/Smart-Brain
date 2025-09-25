1965c139100cb6fa163d5c83ac73ee9cc58f5f5b
Clay <clayton.randall1012@gmail.com>
2025-09-25 16:52:20 +0900

    facerec

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,31 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './FaceRecognition.css';
 
-/*const FaceRecognition = ({ imageUrl, box }) => {
-  return (
-    <div className='center ma'>
-      <div className='absolute mt2'>
-        <img id='inputimage' alt='' src={imageUrl} width='500px' heigh='auto'/>
-        <div className='bounding-box' style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div>
-      </div>
-    </div>
-  );
-}
-
-export default FaceRecognition;*/
-
 const FaceRecognition = ({ imageUrl, boxes }) => {
-  console.log('Boxes:', boxes); /*This was added to determine what Boxes was returning in the array*/
+  useEffect(() => {
+    console.log('Boxes updated:', boxes);
+  }, [boxes]);
+
   return (
     <div className='center ma'>
       <div className='absolute mt2'>
         <img id='inputimage' alt='' src={imageUrl} width='500px' height='auto' />
         {boxes && boxes.map((box, index) => (
-          <div key={index} className='bounding-box' style={{ top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol }}></div>
+          <div
+            key={index}
+            className='bounding-box'
+            style={{
+              top: box.topRow,
+              right: box.rightCol,
+              bottom: box.bottomRow,
+              left: box.leftCol
+            }}
+          ></div>
         ))}
       </div>
     </div>
   );
-}
+};
 
 export default FaceRecognition;
